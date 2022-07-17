a71c8011d6c1f4a5dd89d80ab6a38007f6541371
nishi10000 <34505055+nishi10000@users.noreply.github.com>
2022-07-17 16:00:52 +0900

    yakuzaのアニメーション途中

---
+++ Assets/Scripts/yakuzaAnimationContoroller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yakuzaAnimationContoroller : MonoBehaviour
+{
+    Animator animator;
+
+    // スタート時に呼ばれる
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // フレーム毎に呼ばれる
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetBool("NowShooting", true);
+        }
+        else
+        {
+            animator.SetBool("NowShooting", false);
+        }
+    }
+
+    public void shooterAnimation()
+    {
+        animator.SetBool("NowShooting", true);
+    }
+    public void shooterAnimationEnd()
+    {
+        animator.SetBool("NowShooting", false);
+    }
+}
